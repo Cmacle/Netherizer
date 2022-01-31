0a18b4bbb2e5f0120c0f62a677b226190fb7b7ee
Cmacle <cmacle4500@gmail.com>
2022-01-30 21:49:43 -0500

    Combined file name and file extension, added file size information to the byte list and created int_to_four_byte_list method

--- steg.py
+++ steg.py
@@ -9,7 +9,7 @@ def encode(image_path, file_path, bit_depth):
     having less impact on the image and having less data capacity.
     """
     file_byte_list = file_to_byte_list(file_path, bit_depth)
-    byte_list_to_file(file_byte_list, "")
+#    byte_list_to_file(file_byte_list, "")
     image = Image.open(image_path)
     pixels = image.getdata()
     #print(pixels[1])
@@ -25,23 +25,18 @@ def decode(image, output_path):
 def byte_list_to_file(byte_list, output_path):
     #For testing purposes
     bit_depth = int(byte_list.pop(0).decode())
-    print(bit_depth)
+    print(f'Bit Depth: {bit_depth}')
     file_name_length = int(byte_list.pop(0).decode('UTF-8'))
-    print(file_name_length)
+    print(f'File Name Length: {file_name_length}')
     file_name = []
+
     for num in range(file_name_length):
         file_name.append(byte_list.pop(0).decode('UTF-8'))
-    print(file_name)
-    file_extension_length = int(byte_list.pop(0).decode('UTF-8'))
-    print(file_extension_length)
-    file_extension = []
-    for num in range(file_extension_length):
-        file_extension.append(byte_list.pop(0).decode('UTF-8'))
-    print(file_extension)
-    output_name = "".join(file_name) + "".join(file_extension)
-    print(output_name)
 
-    with open(output_name, "wb") as file:
+    file_name = "".join(file_name)
+    print(f'File Name: {file_name}')
+
+    with open(file_name, "wb") as file:
         print("Writing bytes to file:  ")
         while byte_list:
             file.write(byte_list.pop(0))
@@ -51,9 +46,21 @@ def int_to_byte(x):
     #Turns an int into a string then a byte
     return str(x).encode()
 
-def get_file_extension(file_path):
-    tup = os.path.splitext(file_path)
-    return tup
+def int_to_four_byte_list(x):
+    byte_list = []
+    binary_string = format(x, "b") #Turn the int to a binary string
+    binary_string = binary_string.rjust(32, "0") #pad the string to 32
+    for index in range(0, len(binary_string), 8):
+        #Break the string up into 4 8 character strings
+        byte_list.append(binary_string[index : index + 8])
+    
+    for index, string in enumerate(byte_list):
+        byte_list[index] = int(byte_list[index], 2)
+        byte_list[index] = int_to_byte(byte_list[index])
+
+    return byte_list
+
+
 
 def file_to_byte_list(file_path, bit_depth):
     """
@@ -64,21 +71,19 @@ def file_to_byte_list(file_path, bit_depth):
     """
     byte_list = []   #Declare the Byte list that will be returned
     byte_list.append(int_to_byte(bit_depth))   #Appends the bit depth to the list as a byte
-    file_name, file_extension = get_file_extension(os.path.basename(file_path))   #Get the file name from the path
+    file_name = os.path.basename(file_path)   #Get the file name from the path
     file_name_length = len(file_name)   #Get the length of the file name
     byte_list.append(int_to_byte(file_name_length))   #Appends the length
-
-    print(file_name)
-    print(file_extension)
 
     for letter in file_name: #Append the letters of the name to the list
         byte_list.append(letter.encode())
 
-    file_extension_length = len(file_extension)
-    byte_list.append(int_to_byte(file_extension_length))
-
-    for letter in file_extension:
-        byte_list.append(int_to_byte(letter))
+    file_size = os.path.getsize(file_path) #Get the file size
+    #Turn the file size into a byte list with 4 bytes
+    file_size_byte_list = int_to_four_byte_list(file_size)
+    byte_list.append(file_size_byte_list) #Append the list
+    
+    
 
     with open(file_path, "rb") as file:
         while True:
